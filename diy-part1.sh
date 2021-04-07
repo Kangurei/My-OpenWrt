58a931f3364ab7ef7051f1269eb79bd36560ae82
Kangurei <59930019+Kangurei@users.noreply.github.com>
2021-04-07 12:46:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,11 +20,16 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 
-
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-
-# git clone https://github.com/ITdesk01/jd_openwrt_script.git package/jd_openwrt_script
-
 git clone https://github.com/garypang13/luci-app-bypass.git package/luci-app-bypass
 git clone https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-dnsfilter
 svn co https://github.com/garypang13/openwrt-packages/trunk/smartdns-le package/smartdns-le
+
+svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome                          package/adg
+git clone https://github.com/hyird/Openwrt-AdguardHome.git                                               package/AdGuardHome
+git clone https://github.com/yangsongli/luci-theme-atmaterial.git                                        package/luci-theme-atmaterial
+
+git clone https://github.com/tindy2013/openwrt-subconverter.git                                          package/openwrt-subconverter
+git clone https://github.com/jerrykuku/luci-app-argon-config.git                                         package/luci-app-argon-config
+git clone https://github.com/jerrykuku/lua-maxminddb.git                                                 package/lua-maxminddb
+git clone https://github.com/CCnut/feed-netkeeper.git -b LUCI-LUA-UCITRACK                               package/feed-netkeeper
