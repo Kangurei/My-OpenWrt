fb759a4ff67af2dcbf7c439c2c3971723dab5183
Kangurei <59930019+Kangurei@users.noreply.github.com>
2021-04-20 17:52:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,12 +13,12 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-
+rm -rf feeds/packages/libs/libcap
 rm -rf package/feeds/routing/olsrd/patches/012-gpsd.patch
 rm -rf feeds/packages/net/smartdns
 rm -rf package/lean/luci-theme-argon
 
-
+svn co https://github.com/openwrt/openwrt/trunk/package/libs/libcap                                      feeds/packages/libs/libcap
 svn co https://github.com/Lienol/openwrt-packages/trunk/net/smartdns                                     package/smartdns
 git clone https://github.com/pymumu/luci-app-smartdns.git -b lede                                        package/luci-app-smartdns
 git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06                                     package/luci-theme-argon-jerrykuku
