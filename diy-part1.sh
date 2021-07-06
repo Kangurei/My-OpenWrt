8da4aacf6256f6dfb10ac0ef1d5e2906325381cc
Kangurei <59930019+Kangurei@users.noreply.github.com>
2021-07-06 16:06:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,10 +21,10 @@ sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.de
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 
-git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-git clone https://github.com/yangsongli/luci-theme-atmaterial.git                                        package/luci-theme-atmaterial
+git clone https://github.com/tty228/luci-app-serverchan.git                                              package/luci-app-serverchan
+git clone https://github.com/immortalwrt/luci/tree/master/themes/luci-theme-material.git                 package/luci-theme-atmaterial
 
 git clone https://github.com/tindy2013/openwrt-subconverter.git                                          package/openwrt-subconverter
 git clone https://github.com/jerrykuku/luci-app-argon-config.git                                         package/luci-app-argon-config
 git clone https://github.com/jerrykuku/lua-maxminddb.git                                                 package/lua-maxminddb
-# git clone https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-dnsfilter
+# git clone https://github.com/garypang13/luci-app-dnsfilter.git                                         package/luci-app-dnsfilter
